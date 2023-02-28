79f5893a3e1cf6484ff9dbf419762b4c4d9cb89c
ChetanPatil97 <114873289+ChetanPatil97@users.noreply.github.com>
2023-02-28 16:59:29 +0530

    modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import { ToastContainer} from 'react-toastify'; //copied from npmjs.com/package/react-toastify
 import 'react-toastify/dist/ReactToastify.css'; //copied from npmjs.com/package/react-toastify
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './App.css';
+import UserLogin from './components/UserLogin';
 
 function App() {
   return (
-    <div className="App">
-     <ToastContainer position='top-center' />
-     <h2>Hello React</h2>
-    </div>
+    
+     
+     <Router>
+      <Switch>
+      <Route path='/user/login' exact component = {UserLogin} />
+      </Switch>
+      </Router>
+   
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -2,17 +2,31 @@
  const express = require ("express"); // bring the express
  const app = express();//initilize app 
  const bodyParser = require ("body-parser");
+ const mysql = require("mysql2");
  const cors = require("cors");//to access backend API in react frontend side 
 
+  //establish mysql connection 
+const db = mysql.createPool({
+   host: "localhost",
+   user: "root",
+   password:"My123@7sql",
+   database :"crud_contact",
+}) 
  //middlewares 
  app.use(cors());
  app.use(express.json());
  app.use(bodyParser.urlencoded({extended:true}));
 
  app.get("/", (req,res) => {
-   
-     res.send("Hello Express");
-  });
+    const sqlInsert = "INSERT INTO contact_db (name,email,contact) VALUES ('johnoe','johne@gmail.com',12364)";
+   //  "INSERT INTO admin (adminid, password) VALUES (123 , 'abcd')";
+     db.query(sqlInsert,(error, result)=>{
+        console.log("error", error);
+        console.log("result", result);
+        res.send("Hello Express");
+     });
+   //     // res.send("Hello Express");
+ });
 
   //define the port
   app.listen(5000,() =>{
